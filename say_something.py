39b40fbc76cbffd24fd1b5b3c7d65a358dc7a72a
Geert Jan Bex <geertjan.bex@uhasselt.be>
2023-10-18 14:30:07 +0200

    Add verbosity

--- say_something.py
+++ say_something.py
@@ -11,9 +11,13 @@ def say_bye(name):
     print(f'bye {name}!')
 
 args = utils.handle_cla()
+if args.verbose:
+    print('starting...')
 if args.type == 'hello':
     say_hello(args.name)
 elif args.type == 'bye':
     say_bye(args.name)
 else:
     print('unknown message')
+if args.verbose:
+    print('done...')
--- utils.py
+++ utils.py
@@ -2,5 +2,6 @@ def handle_cla():
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('name', help='name to say hello to')
     arg_parser.add_argument('--type',help='message type')
+    arg_parser.add_argument('--verbose', action='store_true', help='verbose output')
     return arg_parser.parse_args()
 
