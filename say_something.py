e94e417a19a9b960e50c69532f6870275454369d
Geert Jan Bex <geertjan.bex@uhasselt.be>
2023-10-18 10:59:36 +0200

    Replace sys usage by argparse

--- say_something.py
+++ say_something.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
-import sys
+import argparse
 
 
-print(f'hello {sys.argv[1]}!')
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument('name', help='name to say hello to')
+args = arg_parser.parse_args()
+
+print(f'hello {args.name}!')
