6a255af1561ad9b14fc6a591612d79d18e8c33b8
Geert Jan Bex <geertjan.bex@uhasselt.be>
2023-10-18 11:44:00 +0200

    Add bye function and logic for it

--- say_something.py
+++ say_something.py
@@ -6,7 +6,17 @@ import argparse
 def say_hello(name):
     print(f'hello {name}!')
 
+def say_bye(name):
+    print(f'bye {name}!')
+
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('name', help='name to say hello to')
+arg_parser.add_argument('--msg',help='message type')
 args = arg_parser.parse_args()
-say_hello(args.name)
+
+if args.msg == 'hello':
+    say_hello(args.name)
+elif args.msg == 'bye':
+    say_bye(args.name)
+else:
+    print('unknown message')
